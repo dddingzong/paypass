95a6e7e0ee99f61bf641db8077ed1cc96532ff87
USER <cji513@naver.com>
2025-01-15 13:13:31 +0900

    feat(WalletService): WalletService 생성

---
+++ src/main/java/project/paypass/service/WalletService.java
@@ -0,0 +1,19 @@
+package project.paypass.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import project.paypass.domain.Wallet;
+import project.paypass.repository.WalletRepository;
+
+@Service
+@RequiredArgsConstructor
+public class WalletService {
+
+    private final WalletRepository walletRepository;
+
+    @Transactional
+    public void save(Wallet wallet){
+        walletRepository.save(wallet);
+    }
+}
