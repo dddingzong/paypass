50b63a7b952a155d3b32ccdde6a8b0c7012dfd84
USER <cji513@naver.com>
2025-01-15 13:11:31 +0900

    rename(Station): 도매인 이름 변경

--- src/main/java/project/paypass/domain/Station.java
+++ src/main/java/project/paypass/domain/Station.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Entity
-public class station {
+public class Station {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
