95b6f1d8c71bbb5620187c7a468f45d5603bbd94
USER <cji513@naver.com>
2025-01-15 13:10:13 +0900

    fix(Wallet): 변수 이름 변경
    main_id -> mainId

--- src/main/java/project/paypass/domain/Wallet.java
+++ src/main/java/project/paypass/domain/Wallet.java
@@ -4,15 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Entity
-public class wallet {
+public class Wallet {
 
     @Id @GeneratedValue
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private String main_id;
+    private String mainId;
 
     @Column(nullable = false)
     private Long account;
@@ -23,8 +25,8 @@ public class wallet {
     @Column(nullable = false)
     private boolean payCheck;
 
-    public wallet(String main_id) {
-        this.main_id = main_id;
+    public Wallet(String mainId) {
+        this.mainId = mainId;
         this.account = 0L;
         this.charge = 0L;
         this.payCheck = true;
