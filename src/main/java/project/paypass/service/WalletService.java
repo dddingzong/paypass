a50be38c911de983cbe4a0361339f517c7ce402a
USER <cji513@naver.com>
2025-01-16 13:09:55 +0900

    feat(WalletService): findById, findByMainId 메서드 구현

--- src/main/java/project/paypass/service/WalletService.java
+++ src/main/java/project/paypass/service/WalletService.java
@@ -3,6 +3,7 @@ package project.paypass.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import project.paypass.domain.User;
 import project.paypass.domain.Wallet;
 import project.paypass.repository.WalletRepository;
 
@@ -15,5 +16,15 @@ public class WalletService {
     @Transactional
     public void save(Wallet wallet){
         walletRepository.save(wallet);
+    }
+
+    @Transactional
+    public Wallet findById(Long id){
+        return walletRepository.findById(id).get();
+    }
+
+    @Transactional
+    public Long findIdByMainId(String mainId){
+        return walletRepository.findByMainId(mainId).get(0).getId();
     }
 }
