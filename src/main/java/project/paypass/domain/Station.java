70b0d8f80dd0ceb4077bf3af6db9c4254c5cc491
USER <cji513@naver.com>
2025-01-18 18:06:23 +0900

    chore(Station): @Getter 생성

--- src/main/java/project/paypass/domain/Station.java
+++ src/main/java/project/paypass/domain/Station.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Entity
+@Getter
 public class Station {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
