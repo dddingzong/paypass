e0c594775e197c2d947a01e512ad341550945485
USER <cji513@naver.com>
2025-02-09 20:10:03 +0900

    feat(GeofenceLocationRepository): findByMainIdAndStationNumber method 생성

--- src/main/java/project/paypass/repository/GeofenceLocationRepository.java
+++ src/main/java/project/paypass/repository/GeofenceLocationRepository.java
@@ -1,7 +1,14 @@
 package project.paypass.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.paypass.domain.GeofenceLocation;
 
+import java.util.List;
+
 public interface GeofenceLocationRepository extends JpaRepository<GeofenceLocation, Long> {
+
+    @Query("select g from GeofenceLocation g where g.mainId = :mainId AND g.stationNumber = :stationNumber")
+    List<GeofenceLocation> findByMainIdAndStationNumber(@Param("mainId")String mainId, @Param("stationNumber")Long stationNumber);
 }
