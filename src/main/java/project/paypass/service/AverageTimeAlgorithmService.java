5084423a3b74b51e038f854ac4d9fb442104049f
USER <cji513@naver.com>
2025-02-22 11:22:48 +0900

    feat(AverageTimeAlgorithmService): AverageTimeAlgorithmService 틀 생성

---
+++ src/main/java/project/paypass/service/AverageTimeAlgorithmService.java
@@ -0,0 +1,17 @@
+package project.paypass.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import project.paypass.domain.GeofenceLocation;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class AverageTimeAlgorithmService {
+
+    public List<GeofenceLocation> algorithmStart(List<GeofenceLocation> geofenceLocations){
+
+        return geofenceLocations;
+    }
+}
