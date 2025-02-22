3f32dffb55c994231f43189b8f903e1351b86dee
USER <cji513@naver.com>
2025-02-22 19:49:25 +0900

    feat(BasicAlgorithmService): routeId 중복 해결 구현

--- src/main/java/project/paypass/service/BasicAlgorithmService.java
+++ src/main/java/project/paypass/service/BasicAlgorithmService.java
@@ -49,15 +49,10 @@ public class BasicAlgorithmService {
         Map<String, List<Long>> busInfoMap = makeBusInfoMap(busInfoList);
         log.info("busInfoMap = " + busInfoMap);
 
-        // 여기서 하나의 busInfo 안에 두개의 routeId가 있는 경우 해결
-        // ex) 700013, 1400034, 1000030 이런식으로 변형? 000이 포함되어 있으면 routeId 중복 상황이다.
-        Map<String, List<Long>> checkedBusInfoMap = checkDuplicateRouteId(busInfoMap);
-        log.info("checkedBusInfoMap = " + checkedBusInfoMap);
-
         // sequence가 순차적으로 증가하는지 검사
         // sequence의 일정 부분만 조건 만족 시 해당 부분의 index만 추출
         // 조건 만족 시 해당 beginIndex와 endIndex를 가지는 map 추가
-        Map<String, List<Integer>> indexMap = makeIndexMap(checkedBusInfoMap);
+        Map<String, List<Integer>> indexMap = makeIndexMap(busInfoMap);
         log.info("indexMap = " + indexMap);
 
         return indexMap;
@@ -77,7 +72,7 @@ public class BasicAlgorithmService {
     }
 
     private Map<String, List<Long>> makeBusInfoMap(List<String> busInfoList) {
-        Map<String, List<Long>> busInfoMap = new HashMap<>();
+        Map<String, List<Long>> busInfoMap = new TreeMap<>();
 
         // busInfoList에서 routeId만을 추출해서 set 생성
         Set<String> routeIdSet = makeRouteIdSet(busInfoList);
@@ -87,12 +82,17 @@ public class BasicAlgorithmService {
 
         // busInfoMap에서 value값 추가
         busInfoMapPlusValue(busInfoMap, busInfoList);
+        log.info("000이 제거되기 전 busInfoMap" + busInfoMap);
 
-        return busInfoMap;
+        // 중복되는 routeId를 해결하기 위해
+        // sequence 값에 000이 있으면 경우의 수를 두개로 분리
+        Map<String, List<Long>> finalBusInfoMap = divideBusInfoMap(busInfoMap);
+
+        return finalBusInfoMap;
     }
 
     private Map<String, List<Integer>> makeIndexMap(Map<String, List<Long>> busInfoMap) {
-        Map<String, List<Integer>> indexMap = new HashMap<>();
+        Map<String, List<Integer>> indexMap = new TreeMap<>();
 
         for (String routeId : busInfoMap.keySet()) {
             List<Long> sequenceList = busInfoMap.get(routeId);
@@ -138,16 +138,148 @@ public class BasicAlgorithmService {
         // busInfoList를 쪼개서 oneStationInfoList 생성
         for (String oneStationInfo : busInfoList) {
             List<String> oneStationInfoList = Arrays.asList(oneStationInfo.replaceAll("^\\{|\\}$", "").split("},\\{"));
+
+            // oneStationInfoList에 중복의 routeId가 있다면
+            // sequence 값을 000으로 결합
+            // [{100100317,8},{100100307,14},{100100316,24},{100100316,53},{100100308,69}]
+            // -> [{100100317,8},{100100307,14},{100100316,2400053},{100100308,69}]
+            List<String> checkedOneStationInfoList = checkDuplicateRouteId(oneStationInfoList);
+
             // oneStationInfoList를 쪼개서 routeIdAndSequence 생성
-            for (String oneBusInfo : oneStationInfoList) {
+            for (String oneBusInfo : checkedOneStationInfoList) {
                 List<String> routeIdAndSequence = Arrays.asList(oneBusInfo.split(","));
                 String routeId = routeIdAndSequence.get(0);
                 Long sequence = stringToLong(routeIdAndSequence.get(1));
+
                 // sequence 값 추가하기
                 List<Long> sequenceList = busInfoMap.get(routeId);
                 sequenceList.add(sequence);
             }
         }
+    }
+
+    private Map<String, List<Long>> divideBusInfoMap(Map<String, List<Long>> busInfoMap) {
+
+        log.info("===========================================================================");
+        log.info("busInfoMap = " + busInfoMap);
+
+        // 수정하기 위한 복사본 생성
+        Map<String, List<Long>> updatedBusInfoMap = new TreeMap<>(busInfoMap);
+        // 삭제할 key를 저장할 리스트 생성
+        List<String> keysToRemove = new ArrayList<>();
+
+        for (var oneBusInfo : busInfoMap.entrySet()) {
+            String routeId = oneBusInfo.getKey();
+            List<Long> sequenceList = oneBusInfo.getValue();
+
+            for (int i = 0; i < sequenceList.size(); i++) {
+                Long sequence = sequenceList.get(i);
+                String stringSequence = String.valueOf(sequence);
+
+                if (stringSequence.contains("000")) {
+                    List<String> twoSequence = Arrays.asList(stringSequence.split("000"));
+                    Long firstSequence = Long.parseLong(twoSequence.get(0));
+                    Long secondSequence = Long.parseLong(twoSequence.get(1));
+
+                    // 해당 로직이 처음일 경우
+                    if (!routeId.contains("_")) {
+                        sequenceList.set(i, firstSequence);
+                        updatedBusInfoMap.put(routeId + "_1", new ArrayList<>(sequenceList));
+                        sequenceList.set(i, secondSequence);
+                        updatedBusInfoMap.put(routeId + "_2", new ArrayList<>(sequenceList));
+
+                        // 기존 routeId는 삭제해야 하므로 목록에 추가
+                        keysToRemove.add(routeId);
+                        break;
+                    }
+
+                    // 해당 로직이 다회차일 때
+                    if (routeId.contains("_")) {
+                        // ex) 123123123_1 -> [123123123,1]
+                        List<String> routeIdAndCount = Arrays.asList(routeId.split("_"));
+                        String pureRouteId = routeIdAndCount.get(0);
+                        String currentCount = routeIdAndCount.get(1);
+
+                        Long count = findMaxCount(updatedBusInfoMap, pureRouteId);
+
+                        sequenceList.set(i, firstSequence);
+                        updatedBusInfoMap.put(pureRouteId + "_" + currentCount, new ArrayList<>(sequenceList));
+
+                        sequenceList.set(i, secondSequence);
+                        updatedBusInfoMap.put(pureRouteId + "_" + (count+1), new ArrayList<>(sequenceList));
+                        break;
+                    }
+                }
+            }
+        } // for문 종료
+
+        // 기존 맵에서 첫 분리된 key 삭제
+        for (String key : keysToRemove) {
+            updatedBusInfoMap.remove(key);
+        }
+
+        // 원본 맵을 최신 데이터로 갱신
+        busInfoMap.clear();
+        busInfoMap.putAll(updatedBusInfoMap);
+
+        log.info("===========================================================================");
+
+        if(check000(busInfoMap)){
+            return divideBusInfoMap(busInfoMap);
+        }
+
+        return busInfoMap;
+    }
+
+
+    private boolean check000(Map<String, List<Long>> busInfoMap) {
+        for (List<Long> sequenceList : busInfoMap.values()) {
+            for (Long sequence : sequenceList) {
+                if (String.valueOf(sequence).contains("000")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private Long findMaxCount(Map<String, List<Long>> updatedBusInfoMap,String pureRouteId) {
+        Set<String> keySet = updatedBusInfoMap.keySet();
+        ArrayList<Long> countList = new ArrayList<>();
+
+        for (String routeId : keySet) {
+            if (routeId.contains(pureRouteId)){
+                List<String> routeIdAndCount = Arrays.asList(routeId.split("_"));
+                countList.add(Long.parseLong(routeIdAndCount.get(1)));
+            }
+        }
+        return countList.stream().max(Long::compareTo).get();
+    }
+
+    private List<String> checkDuplicateRouteId(List<String> oneStationInfoList) {
+        Map<String, StringBuilder> oneStationInfoMap = new TreeMap<>();
+
+        for (String oneBusInfo : oneStationInfoList) {
+            List<String> routeIdAndSequence = Arrays.asList(oneBusInfo.split(","));
+            String routeId = routeIdAndSequence.get(0);
+            String sequence = routeIdAndSequence.get(1);
+
+            // 같은 routeId가 있으면 "000"을 추가하여 결합
+            oneStationInfoMap.putIfAbsent(routeId, new StringBuilder());
+            if (!oneStationInfoMap.get(routeId).isEmpty()) {
+                oneStationInfoMap.get(routeId).append("000");
+            }
+            oneStationInfoMap.get(routeId).append(sequence);
+        }
+
+        List<String> checkedOneStationInfoList = new ArrayList<>();
+        for (var oneBusInfo : oneStationInfoMap.entrySet()) {
+            String routeId = oneBusInfo.getKey();
+            String sequence = oneBusInfo.getValue().toString();
+            checkedOneStationInfoList.add(routeId + "," + sequence);
+        }
+
+        return checkedOneStationInfoList;
     }
 
     private List<Integer> checkSequential(List<Long> sequenceList) {
@@ -200,6 +332,11 @@ public class BasicAlgorithmService {
             List<GeofenceLocation> containRouteIdList = new ArrayList<>();
             List<Integer> indexList = indexMap.get(routeId);
 
+            // routeId 뒤에 _? 가 존재하는 경우 제거
+            if (routeId.contains("_")) {
+                routeId = routeId.substring(0, routeId.indexOf("_"));
+            }
+
             // sortedGeofenceLocationList에서 geofenceLocation을 추출해서 하나하나 확인하기
             for (GeofenceLocation geofenceLocation : sortedgeofenceLocationList) {
                 // 만약 포함한지 않는다면
@@ -243,15 +380,6 @@ public class BasicAlgorithmService {
             sequentialcontainRouteIdList.addAll(localList);
         }
         return sequentialcontainRouteIdList;
-    }
-
-    private Map<String,List<Long>> checkDuplicateRouteId(Map<String,List<Long>> busInfoMap) {
-        Map<String,List<Long>> checkedBusInfoMap = new HashMap<>();
-
-
-
-
-        return checkedBusInfoMap;
     }
 
     private Long stringToLong(String string) {
