2e45c6ec42e80bbf0cbec0fed18c7d456f41cc2e
USER <cji513@naver.com>
2025-03-15 17:57:13 +0900

    chore(StationRepository): @Repository 추가

--- src/main/java/project/paypass/repository/StationRepository.java
+++ src/main/java/project/paypass/repository/StationRepository.java
@@ -3,8 +3,10 @@ package project.paypass.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import project.paypass.domain.Station;
 
+@Repository
 public interface StationRepository extends JpaRepository<Station, Long> {
 
     @Query("select s.busInfo from Station s where s.stationNumber = :stationNumber")
