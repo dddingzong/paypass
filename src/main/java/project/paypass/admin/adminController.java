2592138656158b22c9d0da4b513dcdc0c734b6c0
USER <cji513@naver.com>
2025-04-10 17:37:43 +0900

    feat(adminController): testAlgorithmTrigger 구현

--- src/main/java/project/paypass/admin/adminController.java
+++ src/main/java/project/paypass/admin/adminController.java
@@ -3,6 +3,7 @@ package project.paypass.admin;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +47,14 @@ public class adminController {
         List<GeofenceLocation> geofenceLocations = geofenceService.findByMainId(mainId);
 
         return ResponseEntity.ok(geofenceLocations);
+    }
+
+    @GetMapping("/test/algorithmTrigger")
+    public void testAlgorithmTrigger(){
+        List<GeofenceLocation> geofenceLocations = geofenceService.findAll();
+        for (GeofenceLocation geofenceLocation : geofenceLocations) {
+            System.out.println("geofenceLocation = " + geofenceLocation);
+        }
+        geofenceService.algorithmTrigger();
     }
 }
