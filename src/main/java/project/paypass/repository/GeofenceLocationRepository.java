71f2cb9ad50a98bac736b645c8179760f57af422
USER <cji513@naver.com>
2025-04-10 17:33:42 +0900

    feat(GeofenceLocationRepository): algorithm trigger를 위한 repository 작성

--- src/main/java/project/paypass/repository/GeofenceLocationRepository.java
+++ src/main/java/project/paypass/repository/GeofenceLocationRepository.java
@@ -12,5 +12,10 @@ public interface GeofenceLocationRepository extends JpaRepository<GeofenceLocati
     @Query("select g from GeofenceLocation g where g.mainId = :mainId AND g.stationNumber = :stationNumber")
     List<GeofenceLocation> findByMainIdAndStationNumber(@Param("mainId")String mainId, @Param("stationNumber")Long stationNumber);
 
+    @Query("SELECT g FROM GeofenceLocation g WHERE g.fenceInTime = (SELECT MAX(g2.fenceInTime) FROM GeofenceLocation g2 WHERE g2.mainId = g.mainId)")
+    List<GeofenceLocation> findMostRecentGeofenceLocationByFenceInTime();
+
     List<GeofenceLocation> findByMainId(String mainId);
+
+    void deleteByMainId(String mainId);
 }
